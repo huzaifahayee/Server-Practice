abbc47cf7369fb6234163b8fc5a15fec2d47bd41
Muhammad Huzaifa <huzaifahayee751@gmail.com>
2026-07-06 13:28:19 +0500

    Server testing

---
+++ server.js
@@ -0,0 +1,8 @@
+// To create server
+
+
+const app = require("./src/app")
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+})
---
+++ src/app.js
@@ -0,0 +1,6 @@
+//server ko create krna
+const express = require("express")
+
+const app = express();
+
+module.exports = app
