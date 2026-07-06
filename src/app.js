66dedfcd7a158e815b59213aab96396f51da1fab
Muhammad Huzaifa <huzaifahayee751@gmail.com>
2026-07-06 15:16:40 +0500

    Testing APIs using POSTMAN

--- src/app.js
+++ src/app.js
@@ -2,5 +2,16 @@
 const express = require("express")
 
 const app = express();
+app.use(express.json())
+const notes = []
+
+app.post('/notes',(req,res) => {
+    notes.push(req.body);
+
+    res.status(201).json({
+        message: "vamos"
+    })
+})
+
 
 module.exports = app
